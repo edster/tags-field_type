92ea8917d0ef8242a9adc6240b06a1f9120e4ee2
Ryan Thompson <service@aiwebsystems.com>
2015-05-22 16:01:01 -0500

    Fix issue where nested array was stored

---
+++ src/TagsFieldTypePresenter.php
@@ -0,0 +1,16 @@
+<?php namespace Anomaly\TagsFieldType;
+
+use Anomaly\Streams\Platform\Addon\FieldType\FieldTypePresenter;
+
+/**
+ * Class TagsFieldTypePresenter
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\TagsFieldType
+ */
+class TagsFieldTypePresenter extends FieldTypePresenter
+{
+
+}
