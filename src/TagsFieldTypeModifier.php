a78434173f7842c59df39eed68b12f6453bef9cb
Ryan Thompson <service@aiwebsystems.com>
2015-06-23 14:56:04 -0500

    Better

--- src/TagsFieldTypeModifier.php
+++ src/TagsFieldTypeModifier.php
@@ -21,7 +21,11 @@ class TagsFieldTypeModifier extends FieldTypeModifier
      */
     public function modify($value)
     {
-        return serialize(array_filter((array)$value));
+        if (!$this->needsModifying($value)) {
+            return $value;
+        }
+
+        return serialize($value);
     }
 
     /**
@@ -32,10 +36,34 @@ class TagsFieldTypeModifier extends FieldTypeModifier
      */
     public function restore($value)
     {
-        if (!$value) {
-            return [];
+        if (!$this->needsRestoring($value)) {
+            return $value;
         }
 
         return unserialize($value);
+    }
+
+    /**
+     * Return whether the value
+     * needs to be modified.
+     *
+     * @param $value
+     * @return bool
+     */
+    protected function needsModifying($value)
+    {
+        return $value && is_array($value);
+    }
+
+    /**
+     * Return whether the value
+     * needs to be resotred.
+     *
+     * @param $value
+     * @return bool
+     */
+    protected function needsRestoring($value)
+    {
+        return $value && is_string($value);
     }
 }
