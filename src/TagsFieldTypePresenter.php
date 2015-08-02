768bc40107287e250077a814c8723392f4fe2cf1
Ryan Thompson <service@aiwebsystems.com>
2015-08-02 12:06:40 -0500

    Presenter

---
+++ src/TagsFieldTypePresenter.php
@@ -0,0 +1,42 @@
+<?php namespace Anomaly\TagsFieldType;
+
+use Anomaly\Streams\Platform\Addon\FieldType\FieldTypePresenter;
+
+/**
+ * Class TagsFieldTypePresenter
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\TagsFieldType
+ */
+class TagsFieldTypePresenter extends FieldTypePresenter
+{
+
+    /**
+     * The decorated object.
+     * This is for IDE support.
+     *
+     * @var TagsFieldType
+     */
+    protected $object;
+
+    /**
+     * Return the tags wrapped in labels.
+     *
+     * @param string $class
+     * @return string
+     */
+    public function labels($class = 'label-default')
+    {
+        return implode(
+            '',
+            array_map(
+                function ($tag) use ($class) {
+                    return '<span class="label ' . $class . '">' . $tag . '</span>';
+                },
+                $this->object->getValue()
+            )
+        );
+    }
+}
