ef9c33f3a18fe1a0a856623cb2eeca20bc14fd7b
Ryan Thompson <service@aiwebsystems.com>
2016-08-28 22:14:13 -0500

    Support string values

--- src/TagsFieldTypePresenter.php
+++ src/TagsFieldTypePresenter.php
@@ -39,4 +39,18 @@ class TagsFieldTypePresenter extends FieldTypePresenter
             )
         );
     }
+
+    /**
+     * Return the string form of the value.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        if (!$value = $this->object->getValue()) {
+            return '';
+        }
+
+        return json_encode($value);
+    }
 }
